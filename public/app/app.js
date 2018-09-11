f0bc0771f6c4df02f6781fc083ac91f7926b33a0
Marcos Cury <marcospcury@gmail.com>
2018-09-10 21:30:21 -0300

    Implementing debounce and takeUntil in a pipe

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,16 @@
-import { handleStatus, log } from "./utils/promise-helpers.js";
+import { pipe, partialize, debounceTime, takeUntil } from "./utils/operators.js";
 import { notasService as service } from "./nota/service.js";
 import './utils/array-helpers.js';
 
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+);
+
 document.querySelector('#myButton')
-  .onclick = () =>
+  .onclick = operations(() =>
     service
       .sumItems('2143')
       .then(console.log)
-      .catch(console.log);
+      .catch(console.log)
+  );
