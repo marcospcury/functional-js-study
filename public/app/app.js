2bf1a829f08e7cf511689cfbdd4e972e297fbc0f
Marcos Cury <marcospcury@gmail.com>
2018-09-10 22:23:18 -0300

    Implementing pub/sub pattern - EventEmitter

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 import { pipe, partialize, debounceTime, takeUntil } from "./utils/operators.js";
 import { timeoutPromise, retry } from "./utils/promise-helpers.js";
 import { notasService as service } from "./nota/service.js";
+import { EventEmitter } from "./utils/event-emitter.js";
 import './utils/array-helpers.js';
 
 const operations = pipe(
@@ -8,9 +9,11 @@ const operations = pipe(
   partialize(debounceTime, 500)
 );
 
+const action = operations(() =>
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itens-totalizados', total))
+    .catch(console.log)
+);
+
 document.querySelector('#myButton')
-  .onclick = operations(() =>
-    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-      .then(console.log)
-      .catch(console.log)
-  );
+  .onclick = action;
---
+++ public/app/console-handler.js
@@ -0,0 +1,3 @@
+import { EventEmitter } from './utils/event-emitter.js';
+
+EventEmitter.on('itens-totalizados', console.log);
